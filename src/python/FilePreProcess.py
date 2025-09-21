0218c10d028ab65496e04799e0fa52f71d88f7b0
18-6-crown <142425917+jgbrzzh@users.noreply.github.com>
2025-09-21 21:47:00 +0800

    delete cpp

--- src/python/FilePreProcess.py
+++ src/python/FilePreProcess.py
@@ -1,1 +1,7 @@
-import
+import os
+def get_filepath(file_path):
+    for root, dirs, files in os.walk(file_path):
+        for file in files:
+            file_path = os.path.join(root, file)
+            preprocess_file(file_path)
+def preprocess_file(file_path):
