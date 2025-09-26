c9f1009db71e5bb97a1c8003966997777bfaf89a
18-6-crown <142425917+jgbrzzh@users.noreply.github.com>
2025-09-26 16:29:13 +0800

    Update working directory and sys.path for src
    Added code to change the working directory to 'src' and append 'src' to sys.path to ensure proper module imports. This replaces the previous method of modifying sys.path[0].

--- tragger.py
+++ tragger.py
@@ -1,7 +1,12 @@
 import src.MainProcess
 import sys
-from pathlib import Path
 
-sys.path[0] = str(Path(sys.path[0]) / "src")
+
+import os
+from pathlib import Path
+sys.path.append(os.path.join(os.path.dirname(__file__), "src"))
+os.chdir(Path(__file__).resolve().parent / "src") #切换工作目录到src 无奈之举
+
+
 src.MainProcess.main()
 
